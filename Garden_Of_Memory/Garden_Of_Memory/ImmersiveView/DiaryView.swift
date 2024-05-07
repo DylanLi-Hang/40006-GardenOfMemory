19b3ad93f2070de9235e6a82abc45b766c986a65
Denni <DennieEom2023@gmail.com>
2024-05-07 14:27:18 +1000

    Update README.md

---
+++ Garden_Of_Memory/Garden_Of_Memory/ImmersiveView/DiaryView.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryView.swift
+//  Garden_Of_Memory
+//
+//  Created by Denni O on 5/7/24.
+//
+
+import SwiftUI
+
+struct DiaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiaryView()
+}
