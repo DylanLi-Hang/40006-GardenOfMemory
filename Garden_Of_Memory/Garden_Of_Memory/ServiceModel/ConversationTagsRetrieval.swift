1e5182f9ea54c96beb00465268e626afef103658
solgrace <solibungrace@gmail.com>
2024-05-10 01:23:05 +1000

    Retrieved mood and conversation tags

--- Garden_Of_Memory/Garden_Of_Memory/ServiceModel/ConversationTagsRetrieval.swift
+++ Garden_Of_Memory/Garden_Of_Memory/ServiceModel/ConversationTagsRetrieval.swift
@@ -1,14 +1,91 @@
+////
+////  ConversationTagsRetrieval.swift
+////  Garden_Of_Memory
+////
+////  Created by Grace on 7/5/2024.
+////
 //
-//  ConversationTagsRetrieval.swift
-//  Garden_Of_Memory
+//import Foundation
+//import OpenAIKit
+//import SwiftUI
+//import GoogleGenerativeAI
 //
-//  Created by Grace on 7/5/2024.
-//
+//class ConversationTagsViewModel: ObservableObject {
+//    
+//    var tagsPrompt: [ChatMessage] = [] // Used for sending prompt to chatgpt asking to rate user's mood from a scale of 1-5
+//    
+//    @Published var tags: [ChatMessage] = []
+//    
+//    var openAI: OpenAI
+//    var gemini: GenerativeModel
+//    
+//    var responseText: String = ""
+//    
+//    // Initilaise another open ai, for your emotion tag retrieval
+//    init() {
+//        let config = Configuration(
+//            organizationId: "",
+//            apiKey: APIKey.openai
+//        )
+//        let geminiConfig = GenerationConfig(
+//            temperature: 0.1,
+//            maxOutputTokens: 2048
+//        )
+//        self.openAI = OpenAI(config)
+//        self.gemini = GenerativeModel(name: "gemini-1.0-pro", apiKey: APIKey.default, generationConfig: geminiConfig)
+//        
+//        self.tagsPrompt.append(ChatMessage(role: .system, content: Prompt.tagsInitPrompt))
+//    }
+//    
+//    func retrieveConversationTags(_ latestFourUserMessagesContent: [String], completion: @escaping ([String]?, Error?) -> Void) {
+//        let latestFourConversations = latestFourUserMessagesContent.joined(separator: "\n")
+//        self.tagsPrompt.append(ChatMessage(role: .user, content: latestFourConversations))
+//        
+//        Task {
+//            do {
+//                let result = try await openAI.generateChatCompletion(parameters: ChatParameters(model: .chatGPTTurbo, messages: self.tagsPrompt))
+////                let result = try await gemini.generateContent(Prompt.tagsInitPrompt + latestFourConversations)
+//                var tags = [String]()
+//                
+//                guard let choice = result.choices.first else { return }
+//                if let message = choice.message, let content = message.content {
+//                    if let startIndex = content.range(of: "{"), let endIndex = content.range(of: "}") {
+//                        let tagSubstring = content[startIndex.upperBound..<endIndex.lowerBound]
+//                        let components = tagSubstring.components(separatedBy: ":")
+//                        if components.count >= 2 {
+//                            let word = components[1].trimmingCharacters(in: .whitespaces)
+//                                .replacingOccurrences(of: "\"", with: "")
+//                                .replacingOccurrences(of: "'", with: "")
+//                            print("Conversation Tag:", word)
+//                            tags.append(word)
+//                        } else {
+//                            completion(nil, nil)
+//                            return
+//                        }
+//                    }
+//                }
+//                
+//                completion(tags, nil)
+//            } catch {
+//                completion(nil, error)
+//            }
+//        }
+//    }
+//    
+//}
+
+
+
+
+
+
+
+
+
 
 import Foundation
 import OpenAIKit
 import SwiftUI
-import GoogleGenerativeAI
 
 class ConversationTagsViewModel: ObservableObject {
     
@@ -17,7 +94,6 @@ class ConversationTagsViewModel: ObservableObject {
     @Published var tags: [ChatMessage] = []
     
     var openAI: OpenAI
-    var gemini: GenerativeModel
     
     var responseText: String = ""
     
@@ -27,48 +103,53 @@ class ConversationTagsViewModel: ObservableObject {
             organizationId: "",
             apiKey: APIKey.openai
         )
-        let geminiConfig = GenerationConfig(
-            temperature: 0.1,
-            maxOutputTokens: 2048
-        )
         self.openAI = OpenAI(config)
-        self.gemini = GenerativeModel(name: "gemini-1.0-pro", apiKey: APIKey.default, generationConfig: geminiConfig)
         
         self.tagsPrompt.append(ChatMessage(role: .system, content: Prompt.tagsInitPrompt))
     }
     
-    func retrieveConversationTags(_ latestFourUserMessagesContent: [String], completion: @escaping ([String]?, Error?) -> Void) {
+    func retrieveConversationTags(_ latestFourUserMessagesContent: [String]) {
         let latestFourConversations = latestFourUserMessagesContent.joined(separator: "\n")
+
         self.tagsPrompt.append(ChatMessage(role: .user, content: latestFourConversations))
         
-        Task {
-            do {
-                let result = try await openAI.generateChatCompletion(parameters: ChatParameters(model: .chatGPTTurbo, messages: self.tagsPrompt))
-//                let result = try await gemini.generateContent(Prompt.tagsInitPrompt + latestFourConversations)
-                var tags = [String]()
-                
-                guard let choice = result.choices.first else { return }
-                if let message = choice.message, let content = message.content {
-                    if let startIndex = content.range(of: "{"), let endIndex = content.range(of: "}") {
-                        let tagSubstring = content[startIndex.upperBound..<endIndex.lowerBound]
-                        let components = tagSubstring.components(separatedBy: ":")
-                        if components.count >= 2 {
-                            let word = components[1].trimmingCharacters(in: .whitespaces)
-                                .replacingOccurrences(of: "\"", with: "")
-                                .replacingOccurrences(of: "'", with: "")
-                            tags.append(word)
-                        } else {
-                            completion(nil, nil)
-                            return
+        do {
+            // Perform the asynchronous operation
+            Task {
+                do {
+                    let result = try await openAI.generateChatCompletion(parameters: ChatParameters(model: .chatGPTTurbo, messages: self.tagsPrompt))
+                    
+                    // Process the result synchronously
+                    for choice in result.choices {
+                        
+                        if let message = choice.message, let content = message.content {
+                            
+                            // Find the index of the opening and closing curly braces
+                            if let startIndex = content.range(of: "{"), let endIndex = content.range(of: "}") {
+                                // Extract the substring containing the mood value
+                                let tagSubstring = content[startIndex.upperBound..<endIndex.lowerBound]
+ 
+                                // Split the substring at the colon
+                                let components = tagSubstring.components(separatedBy: ":")
+                                if components.count >= 2 {
+                                    // Extract the second component (the word), trimming whitespace
+                                    let word = components[1].trimmingCharacters(in: .whitespaces).replacingOccurrences(of: "\"", with: "").replacingOccurrences(of: "'", with: "")
+                                    print("Conversation Tag:", word)
+                                    // Use the word value as needed
+                                } else {
+                                    print("Invalid tag substring format")
+                                }
+                            }
                         }
                     }
+                } catch {
+                    print("Error processing text: \(error)")
                 }
-                
-                completion(tags, nil)
-            } catch {
-                completion(nil, error)
             }
+        } catch {
+            print("Error processing text: \(error)")
         }
+        
     }
     
 }
--- Garden_Of_Memory/Garden_Of_Memory/ServiceModel/EmotionScaleRetrieval.swift
+++ Garden_Of_Memory/Garden_Of_Memory/ServiceModel/EmotionScaleRetrieval.swift
@@ -1,9 +1,85 @@
+////
+////  EmotionScaleRetrieval.swift
+////  Garden_Of_Memory
+////
+////  Created by Grace on 7/5/2024.
+////
 //
-//  EmotionScaleRetrieval.swift
-//  Garden_Of_Memory
+//import Foundation
+//import OpenAIKit
+//import SwiftUI
 //
-//  Created by Grace on 7/5/2024.
+//class EmotionScaleViewModel: ObservableObject {
+//    
+//    var moodPrompt: [ChatMessage] = [] // Used for sending prompt to chatgpt asking to rate user's mood from a scale of 1-5
+//    @Published var mood: Int = 0 // Used for receiving the mood value from chatgpt's response
+//        
+//    var openAI: OpenAI
+//    
+//    var responseText: String = ""
+//    
+//    // Initilaise another open ai, for your emotion tag retrieval
+//    init() {
+//        let config = Configuration(
+//            organizationId: "",
+//            apiKey: APIKey.openai
+//        )
+//        self.openAI = OpenAI(config)
+//                
+//        self.moodPrompt.append(ChatMessage(role: .system, content: Prompt.emotionScaleInitPrompt))
+//    }
+//    
+//    func retrieveEmotionScale(_ latestFourUserMessagesContent: [String], completion: @escaping (Int?, Error?) -> Void) {
+//        print("meow 1")
+//        let latestFourConversations = latestFourUserMessagesContent.joined(separator: "\n")
+//        self.moodPrompt.append(ChatMessage(role: .user, content: latestFourConversations))
 //
+//        Task {
+//            do {
+//                print("meow 2")
+//                print("self.moodPrompt:", self.moodPrompt)
+//                let result = try await openAI.generateChatCompletion(parameters: ChatParameters(model: .chatGPTTurbo, messages: self.moodPrompt))
+//                
+//                print("meow 3")
+//                
+//                // Process the result synchronously
+//                guard let choice = result.choices.first else { return }
+//                if let message = choice.message, let content = message.content {
+//                    print("meow 4")
+//                    // Find the index of the opening and closing curly braces
+//                    if let startIndex = content.range(of: "{"), let endIndex = content.range(of: "}") {
+//                        print("meow 5")
+//                        // Extract the substring containing the mood value
+//                        let moodSubstring = content[startIndex.upperBound..<endIndex.lowerBound]
+//                        
+//                        // Trim whitespace and remove any non-numeric characters
+//                        let trimmedMoodString = moodSubstring.trimmingCharacters(in: .whitespaces).filter { "0123456789".contains($0) }
+//                        
+//                        // Convert the trimmed mood string to an integer
+//                        if let mood = Int(trimmedMoodString) {
+//                            print("Mood:", mood)
+//                            completion(mood, nil)
+//                            return
+//                        }
+//                    }
+//                }
+//            } catch {
+//                print("Error processing text: \(error)")
+//                completion(nil, error)
+//            }
+//        }
+//    }
+//    
+//}
+
+
+
+
+
+
+
+
+
 
 import Foundation
 import OpenAIKit
@@ -29,36 +105,43 @@ class EmotionScaleViewModel: ObservableObject {
         self.moodPrompt.append(ChatMessage(role: .system, content: Prompt.emotionScaleInitPrompt))
     }
     
-    func retrieveEmotionScale(_ latestFourUserMessagesContent: [String], completion: @escaping (Int?, Error?) -> Void) {
+    func retrieveEmotionScale(_ latestFourUserMessagesContent: [String]) {
         let latestFourConversations = latestFourUserMessagesContent.joined(separator: "\n")
+        
         self.moodPrompt.append(ChatMessage(role: .user, content: latestFourConversations))
-
+                
+        do {
+            // Perform the asynchronous operation
             Task {
                 do {
                     let result = try await openAI.generateChatCompletion(parameters: ChatParameters(model: .chatGPTTurbo, messages: self.moodPrompt))
-                    
+                                        
                     // Process the result synchronously
-                    guard let choice = result.choices.first else { return }
-                    if let message = choice.message, let content = message.content {
-                        // Find the index of the opening and closing curly braces
-                        if let startIndex = content.range(of: "{"), let endIndex = content.range(of: "}") {
-                            // Extract the substring containing the mood value
-                            let moodSubstring = content[startIndex.upperBound..<endIndex.lowerBound]
-                            
-                            // Trim whitespace and remove any non-numeric characters
-                            let trimmedMoodString = moodSubstring.trimmingCharacters(in: .whitespaces).filter { "0123456789".contains($0) }
-                            
-                            // Convert the trimmed mood string to an integer
-                            if let mood = Int(trimmedMoodString) {
-                                completion(mood, nil)
-                                return
+                    for choice in result.choices {
+                        
+                        if let message = choice.message, let content = message.content {
+                            // Find the index of the opening and closing curly braces
+                            if let startIndex = content.range(of: "{"), let endIndex = content.range(of: "}") {
+                                // Extract the substring containing the mood value
+                                let moodSubstring = content[startIndex.upperBound..<endIndex.lowerBound]
+                                
+                                // Trim whitespace and remove any non-numeric characters
+                                let trimmedMoodString = moodSubstring.trimmingCharacters(in: .whitespaces).filter { "0123456789".contains($0) }
+                                
+                                // Convert the trimmed mood string to an integer
+                                if let mood = Int(trimmedMoodString) {
+                                    print("Mood:", mood)
+                                    // Use the mood value as needed
+                                }
                             }
                         }
                     }
                 } catch {
                     print("Error processing text: \(error)")
-                    completion(nil, error)
                 }
             }
+        } catch {
+            print("Error processing text: \(error)")
+        }
     }
 }
