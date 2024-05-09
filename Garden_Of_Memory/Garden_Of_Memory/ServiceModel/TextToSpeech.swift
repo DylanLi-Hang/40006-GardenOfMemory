a2431699a712f5b9678e71ec724cf233178a2f6d
solgrace <solibungrace@gmail.com>
2024-05-09 21:23:57 +1000

    Change status when speaking

--- Garden_Of_Memory/Garden_Of_Memory/ServiceModel/TextToSpeech.swift
+++ Garden_Of_Memory/Garden_Of_Memory/ServiceModel/TextToSpeech.swift
@@ -9,42 +9,46 @@ import Foundation
 import AVFoundation
 
 class TextToSpeechViewModel: NSObject, ObservableObject {
-    
-    // Create an utterance.
-    let utterance = AVSpeechUtterance(string: "The quick brown fox jumped over the lazy dog.")
-
-    // Configure the utterance.
-    override init() {
-        super.init()
         
-//        utterance.rate = 0.57
-        utterance.rate = AVSpeechUtteranceDefaultSpeechRate // Normal speech rate
-        utterance.pitchMultiplier = 1.1
-        utterance.postUtteranceDelay = 0.2
-        utterance.volume = 0.7
-
-        // Retrieve the British English voice.
-        let voice = AVSpeechSynthesisVoice(language: "en-GB")
-        
-        // Assign the voice to the utterance.
-        utterance.voice = voice
-    }
-    
     // Create a speech synthesizer.
     let synthesizer = AVSpeechSynthesizer()
+    let viewModel = ViewModel.shared
     
-//    // Method to trigger speech synthesis.
-//    func speak() {
+    override init() {
+        super.init()
+        synthesizer.delegate = self
+    }
+        
+//    // Method to trigger speech synthesis with specified text.
+//    func speak(_ text: String) {
+//        let utterance = AVSpeechUtterance(string: text)
+//        
+//        // Retrieve the British English voice.
+//        let voice = AVSpeechSynthesisVoice(language: "en-GB")
+//        
+//        // Assign the voice to the utterance.
+//        utterance.voice = voice
+//        
+//        // Set other properties as needed, like rate, pitch, and volume.
+//        utterance.rate = AVSpeechUtteranceDefaultSpeechRate // Normal speech rate
+//        utterance.pitchMultiplier = 1.1
+//        utterance.postUtteranceDelay = 0.2
+//        utterance.volume = 0.7
+//        
 //        // Tell the synthesizer to speak the utterance.
 //        synthesizer.speak(utterance)
 //    }
     
-    // Method to trigger speech synthesis with specified text.
-    func speak(_ text: String) {
-        let utterance = AVSpeechUtterance(string: text)
-        
-        // Tell the synthesizer to speak the utterance.
+    func speak(_ string: String) {
+        let utterance = AVSpeechUtterance(string: string)
         synthesizer.speak(utterance)
     }
     
+}
+
+extension TextToSpeechViewModel: AVSpeechSynthesizerDelegate {
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        print("all done")
+        viewModel.status = .idle
+    }
 }
