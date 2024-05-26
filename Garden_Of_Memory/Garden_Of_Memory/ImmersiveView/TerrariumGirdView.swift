88c63c304ca8a9078b3fed874aaa216a2e19c0a8
Denni <DennieEom2023@gmail.com>
2024-05-26 23:58:15 +1000

    Added TerrariumGirdView

---
+++ Garden_Of_Memory/Garden_Of_Memory/ImmersiveView/TerrariumGirdView.swift
@@ -0,0 +1,18 @@
+//
+//  TerrariumGirdView.swift
+//  Garden_Of_Memory
+//
+//  Created by Denni O on 5/27/24.
+//
+
+import SwiftUI
+
+struct TerrariumGirdView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TerrariumGirdView()
+}
